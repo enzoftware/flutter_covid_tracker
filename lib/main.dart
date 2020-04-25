9efc18363f731572ae77d3d566884f121999c500
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-24 19:50:15 -0500

    :tada: error handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_covid_tracker/ui/screens/home_screen.dart';
+import 'package:flutter_covid_tracker/ui/widgets/error_screen.dart';
 
 void main() {
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Material(child: ErrorScreen());
+  };
   runApp(MyApp());
 }
 
@@ -12,8 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'COVID-19 Tracker',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        backgroundColor: Colors.deepPurple,
+        primarySwatch: Colors.indigo,
         accentColor: Colors.deepOrange,
       ),
       home: HomeScreen(),
